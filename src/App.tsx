4f76b9b9e525912aa85af5c8763ef2a5957bfcab
Misha Kanyuka <m.kwnyuka.m2005@gmail.com>
2023-08-14 13:29:16 +0200

    added lazy Loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
+import React from 'react'
 import { Routes, Route } from "react-router-dom";
+import Loadable from 'react-loadable';
 
 import "./scss/app.scss";
 import Home from "./pages/Home";
-import NotFound from "./pages/NotFound";
-import Cart from "./pages/Cart";
-import FullPizza from "./pages/FullPizza";
 import MainLayout from "./layouts/MainLayout";
+import Loading from './components/Loading';
+
+const Cart = Loadable({
+  loader: () => import(/* webpackChunkName: "Cart" */"./pages/Cart"),
+  loading: Loading
+})
+const FullPizza = React.lazy(() => import(/* webpackChunkName: "FullPizza" */"./pages/FullPizza"))
+const NotFound = React.lazy(() => import(/* webpackChunkName: "NotFound" */"./pages/NotFound"))
 
 function App() {
   return (
@@ -13,8 +20,14 @@ function App() {
       <Route path="/" element={<MainLayout />}>
         <Route path="" element={<Home />} />
         <Route path="cart" element={<Cart />} />
-        <Route path="pizza/:id" element={<FullPizza />} />
-        <Route path="*" element={<NotFound />} />
+        <Route path="pizza/:id" element={
+          <React.Suspense fallback={<Loading />}>
+            <FullPizza />
+          </React.Suspense>} />
+        <Route path="*" element={
+          <React.Suspense fallback={<Loading />}>
+            <NotFound />
+          </React.Suspense>} />
       </Route>
     </Routes>
   );
---
+++ src/components/Loading.tsx
@@ -0,0 +1,7 @@
+import { FC } from "react"
+
+const Loading: FC = () => {
+  return <h2 style={{textAlign: "center"}}>Loading...</h2>
+}
+
+export default Loading
